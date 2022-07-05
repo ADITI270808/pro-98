ba592c638a2e2038f0fe3656defd0ad189218373
ADITI270808 <79738392+ADITI270808@users.noreply.github.com>
2022-07-05 19:00:56 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,14 @@
+import random
+number = random.randint(1,9)
+
+a=input("guess the random number between 1 to 9")
+print (a)
+
+    if (a < numberprint ):
+        print("guess was too low")
+
+    elif(a > numberprint):
+        print("guess was too high")
+
+    else:
+        ("Congratulations you guessed it correct") 
